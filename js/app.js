6f94f4b8f7c2be5dc669350456d84681e3a82b83
cristy22mx <cristy22mx@gmail.com>
2018-01-21 23:55:57 -0600

    avance 1

---
+++ js/app.js
@@ -0,0 +1,33 @@
+ 
+
+
+
+
+
+
+
+
+
+//Habilitar el boton NEXT
+ //Traer elementos del HTML
+  // var $nameInput = $("#name-input");
+  // var $phoneInput = $("#phone-input");
+  // var contacts = [];
+
+  // function loadPage () {
+  //   $(".modal").modal();
+  //   $("# form").submit(addContact);
+  //   $nameInput.keyup(validateContact);
+  //   $phoneInput.keyup(validateContact);
+  // };
+
+  // function validateContact () {
+  //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
+  //   var $containerAddContact = $("#add-contact");
+  //   if($(this).val().trim().length >0 ) {
+  //     $containerAddContact.removeAttr("disabled");
+  //   }else {
+  //     $containerAddContact.attr("disabled", true);
+  //   }
+  // };
+
